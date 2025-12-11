43467c2c84aa79695f680b798b464be12479d06c
mehmet ali <mehme@TEDESCO420>
2025-12-11 20:15:56 +0300

    .

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/ICommandHandler.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+
+namespace BuildingBlocks.CQRS
+{
+    // Command Handler that does not return a value
+    public interface ICommandHandler<in TCommand> :
+                          ICommandHandler<TCommand, Unit>
+                    where TCommand : ICommand<Unit>
+    {
+    }
+    // Command Handler that returns a value
+    public interface ICommandHandler<in TCommand, TResponse> : IRequestHandler<TCommand, TResponse>
+                    where TCommand : ICommand<TResponse>
+                    where TResponse : notnull
+    {
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+
+namespace Catalog.API.Products.CreateProduct
+{
+    public record CreateProductCommand(
+        string Name,
+        string Description,
+        string ImageFile,
+        decimal Price,
+        List<string> Category) : IRequest<CreateProductResult>;
+    public record CreateProductResult(Guid Id);
+    internal class CreateProductHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+    {
+        public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+}
