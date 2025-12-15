a4948995c9d7a694d42395d0d72fe98fa099c64c
mehmet ali <mehme@TEDESCO420>
2025-12-16 02:02:02 +0300

    getProducts as paged

--- src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
@@ -1,13 +1,17 @@
-﻿namespace Catalog.API.Products.GetProducts;
-public record GetProductsQuery() : IQuery<GetProductsResult>;
+﻿using Marten.Pagination;
+
+namespace Catalog.API.Products.GetProducts;
+public record GetProductsQuery(int PageNumber, int PageSize) : IQuery<GetProductsResult>;
 public record GetProductsResult(IEnumerable<Product> Products);
 
 public class GetProductsQueryHandler(IDocumentSession documentSession) : IQueryHandler<GetProductsQuery, GetProductsResult>
 {
     public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
     {
-        var products = await documentSession.Query<Product>().ToListAsync(cancellationToken);
+        var products = await documentSession
+            .Query<Product>()
+            .ToPagedListAsync(query.PageNumber, query.PageSize, cancellationToken);
 
-        return new GetProductsResult(products);
+        return new(products);
     }
 }
